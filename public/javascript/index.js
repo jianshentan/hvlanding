f2f442db184cfb101620501bbd368e0819666de7
jianshentan <jianshentan@gmail.com>
2016-09-12 04:21:41 +0000

    first

---
+++ public/javascript/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $('body').mousemove(function(event) {
+    // coords (event.pageX, event.pageY)
+    $(".hv-cursor").offset({ top: event.pageY - 40, left: event.pageX - 80});
+  });
+  
+  $('body').keydown(function(e) {
+    var overlay = $(".hv-overlay");
+    var page = $(".hv-page");
+    if(e.keyCode == 37) { // left
+    
+      overlay.addClass('blue');
+      overlay.css("left", "100vw");
+      overlay.show();
+      overlay.animate({ "left": "0px" });
+      page.animate({ "left": "-100vw"}, function() {
+        window.location.href = "http://www.hellovelocity.com";
+      });
+      
+    } else if(e.keyCode == 39) { // right
+      
+      overlay.addClass('white');
+      overlay.css("left", "-100vw");
+      overlay.show();
+      overlay.animate({ "left": "0px" });
+      page.animate({ "left": "100vw"}, function() {
+        window.location.href = "http://design.hellovelocity.com";
+      });
+      
+    }
+  });
+});
